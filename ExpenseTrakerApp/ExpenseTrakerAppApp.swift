fd1273b3eaa0e2d9b00d9714e7a915ab2477dea6
Cristian Laguna <cristiandavidlaguna.97@gmail.com>
2024-04-29 12:56:34 -0500

    Initial Commit

---
+++ ExpenseTrakerApp/ExpenseTrakerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrakerAppApp.swift
+//  ExpenseTrakerApp
+//
+//  Created by Cristian David Laguna Aldana on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrakerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
